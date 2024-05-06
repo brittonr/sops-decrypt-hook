f0c90f773c50ccf6313ec0ee514a8d37845a7fc4
Britton Robitzsch <b@robitzs.ch>
2024-05-06 12:29:00 -0600

    Update sops-decrypt-hook.nix

--- sops-decrypt-hook.nix
+++ sops-decrypt-hook.nix
@@ -1,13 +1,16 @@
-{ sopsFile }: {
+{sopsFiles}: {
   shellHook = ''
-    if [ -f ${sopsFile} ]; then
-      while IFS= read -r line; do
-        if [[ "$line" =~ ^[A-Za-z_][A-Za-z0-9_]*:.*$ ]]; then
-          key=$(echo "$line" | cut -d ':' -f 1)
-          value=$(echo "$line" | cut -d ':' -f 2- | sed 's/^ *//')
-          export $key="$value"
-        fi
-      done < <(sops -d ${sopsFile})
-    fi
+    for sopsFile in ${toString sopsFiles}; do
+      if [ -f "$sopsFile" ]; then
+        echo "Decrypting $sopsFile"
+        while IFS= read -r line; do
+          if [[ "$line" =~ ^[A-Za-z_][A-Za-z0-9_]*=.*$ ]]; then
+            key=$(echo "$line" | cut -d '=' -f 1)
+            value=$(echo "$line" | cut -d '=' -f 2)
+            export $key="$value"
+          fi
+        done < <(sops --decrypt $sopsFile)
+      fi
+    done
   '';
 }
